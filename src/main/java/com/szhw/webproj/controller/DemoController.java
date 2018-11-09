fd0aec427dc6447c1795cdbe7d071cb83ff6f4ed
XianyingWang2018 <972340199@qq.com>
2018-11-09 21:59:46 +0800

    wangxianying join in

--- src/main/java/com/szhw/webproj/controller/DemoController.java
+++ src/main/java/com/szhw/webproj/controller/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/")
     public String index() {
-        return "HELLO HELLO";
+        return "HELLO HELLO wangxianyinglaile";
     }
 }
